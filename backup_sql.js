d21e7203b206020cd3639b35d727fb087cb98836
flamendless <flamendless8@gmail.com>
2021-01-28 17:24:45 +0800

    implemented creation of backups folder if it does not exist

--- backup_sql.js
+++ backup_sql.js
@@ -4,8 +4,14 @@ const filename = `${Math.round(Date.now()/1000)}.dump.sql`;
 const dir = "backups";
 const username = "root";
 
-const cmd = `/opt/lampp/bin/mysqldump -u ${username} chazydes > ${dir}/${filename}`;
+if (!fs.existsSync(dir)) {
+	fs.mkdir(dir, function(err) {
+		if (err) console.log(`Unable to create ${dir}`);
+		else console.log(`Created ${dir} successfuly!`);
+	});
+}
 
+const cmd = `/opt/lampp/bin/mysqldump -u ${username} chazydes > ${dir}/${filename}`;
 const dump = child_process.exec(cmd, [], function(err, stdout) {
 		if (err) console.log(err);
 		else console.log(stdout);
